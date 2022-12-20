943a8155c5dca89d77b1cfba532e8e1a3cb98e8f
emidiaz00 <emildsn00@gmail.com>
2022-12-20 09:52:21 -0300

    add project

---
+++ src/components/Counter.vue
@@ -0,0 +1,39 @@
+<template>
+  <h1>Counter</h1>
+  <p>  {{counter}} <sup>2</sup>  = {{ counter * counter}} </p>
+  <p>  {{counter}} <sup>2</sup>  = {{ getFinalValue() }} </p>
+  <p>  {{counter}} <sup>2</sup>  = {{ getSquareCounter }} </p>
+</template>
+
+
+<script>
+export default {
+
+    data() {
+        return {
+           counter: 5 
+        }
+    },
+    methods: {
+        getFinalValue() {
+            return this.counter * this.counter
+        } 
+    },
+    computed: {
+        getSquareCounter() {
+            console.log('computed square counter');
+            return this.counter + this.counter
+        }
+    }
+
+}
+</script>
+
+<style>
+p {
+    color: black;
+    font-weight: bold;
+    font-size: 40px;
+}
+
+</style>
