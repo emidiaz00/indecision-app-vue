bc5807e55393960d6bff57999c99522e7876b2ff
emildsn <emildsn00@gmail.com>
2023-01-11 23:05:24 -0300

    add test increase
    * add test increase value
    
    * fix test increase

--- tests/unit/components/counter.spec.js
+++ tests/unit/components/counter.spec.js
@@ -6,5 +6,21 @@ describe('Counter component', () => {
         const wrapper = shallowMount(Counter)
         expect(wrapper.html()).toMatchSnapshot()
      })
+     test('should increase value of counter', async () => { 
+        const wrapper = shallowMount(Counter)
+        const increaseBtn = wrapper.find('button')
+
+        increaseBtn.trigger('click')
+        let value = wrapper.find('[data-testid="counter"]').text()
+        expect(value).toBe('100')
+
+        const decreaseBtn = wrapper.findAll('button')[1]
+
+        await decreaseBtn.trigger('click')
+        await decreaseBtn.trigger('click')
+
+        value = wrapper.find('[data-testid="counter"]').text()
+        expect(value).toBe('99')
+      })
 });
 
